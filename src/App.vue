a1b39f1a28105638663a3edee14dccb1ca024c53
babahironobu <ghdtjs0417@gmail.com>
2021-11-05 13:15:04 +0900

    Add: 설치 확인 모달

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,50 @@
 <template>
   <ion-app>
     <ion-router-outlet />
+
+    <ion-alert
+      :is-open="isOpen"
+      header="새로운 버전을 설치합니다."
+      :message="`${downloadProgress}%`"
+      :buttons="buttons"
+      @didDismiss="setOpen(false)"
+    />
   </ion-app>
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue';
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { ref, onMounted } from 'vue';
+import {
+  IonApp, IonRouterOutlet, IonAlert, AlertButton,
+} from '@ionic/vue';
 import { Capacitor } from '@capacitor/core';
 import { Deploy } from 'cordova-plugin-ionic';
+
+const isOpen = ref(false);
+const setOpen = (v = true) => {
+  isOpen.value = v;
+};
+
+const downloadProgress = ref(0);
+const setDownloadProgress = (v: number) => {
+  downloadProgress.value = v;
+};
+
+const buttons: AlertButton[] = [
+  {
+    text: '확인',
+    handler: async () => {
+      await Deploy.downloadUpdate((progress) => {
+        if (!progress) {
+          return;
+        }
+        setDownloadProgress(progress);
+      });
+      await Deploy.extractUpdate();
+      await Deploy.reloadApp();
+    },
+  },
+];
 
 onMounted(async () => {
   if (!Capacitor.isNativePlatform) {
@@ -17,14 +53,6 @@ onMounted(async () => {
 
   const update = await Deploy.checkForUpdate();
 
-  if (update.available) {
-    await Deploy.downloadUpdate((progress) => {
-      console.log('download progress:', progress);
-    });
-    await Deploy.extractUpdate((progress) => {
-      console.log('extract progress:', progress);
-    });
-    await Deploy.reloadApp();
-  }
+  setOpen(update.available);
 });
 </script>
